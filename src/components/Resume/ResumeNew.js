015e5ca86c1d79585168ab5e4d6da0bc7f7c964e
miyuou <164646166+miyuou@users.noreply.github.com>
2025-03-16 19:37:07 +0000

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdfEnglish from "../../Assets/CV Mirghany Meryem.pdf";
-import pdfFrench from "../../Assets/CV Meryem Mirghany.pdf";
+import pdfFrench from "../../Assets/CV meryem mirghany.pdf";
 import { AiOutlineEye } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
