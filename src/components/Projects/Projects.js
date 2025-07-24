52c1689c794fabf5f4a67c305c2b3d9fb2395dff
mery <meryem.mirghany03@gmail.com>
2025-07-24 12:50:11 +0100

    prjct update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ import Collection from "../../Assets/Projects/coll.png";
 import Hiber from "../../Assets/Projects/hotel hiber casablanca.png";
 import emsiexchange from "../../Assets/Projects/STICKER LOGO.png";
 import auction from "../../Assets/Projects/auction.jpeg";
-import PowerGLPI from "../../Assets/Projects/pwglpi.jpeg";
+import PowerGLPI from "../../Assets/Projects/pwglpi.jpg";
 function Projects() {
   return (
     <Container fluid className="project-section">
