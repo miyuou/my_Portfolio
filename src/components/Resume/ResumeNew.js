6b16ff4ffbaeca9a13b789e86f6029406b5aff5d
mery <meryem.mirghany03@gmail.com>
2025-09-10 01:18:55 +0100

    d

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -12,7 +12,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
-  const [selectedResume, setSelectedResume] = useState("french");
+  const [selectedResume, setSelectedResume] = useState("english");
 
   useEffect(() => {
     setWidth(window.innerWidth);
