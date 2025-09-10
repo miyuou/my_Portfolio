e3bee93a8a37e4079606aa2af0ddd4cd3de21261
mery <meryem.mirghany03@gmail.com>
2025-09-10 01:04:21 +0100

    u

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdfEnglish from "../../Assets/CVVV.pdf";
+import pdfEnglish from "../../Assets/Mirghany_Meryem_CV.pdf";
 import pdfFrench from "../../Assets/Meryem_Mirghany_CV.pdf";
 import { AiOutlineEye, AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
